7934a2b15504f031a7f0accfe13917a6bf7b4d13
Huy <huy@Huys-MacBook-Pro.local>
2022-05-07 17:21:11 +0700

    update code

--- App.js
+++ App.js
@@ -1,17 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,LogBox } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, LogBox } from "react-native";
 import RootStack from "./app/navigation/rootStack";
-import { store } from './app/Redux/store'
-import { Provider } from 'react-redux'
+import { store } from "./app/Redux/store";
+import { Provider } from "react-redux";
+import React, { useEffect } from "react";
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
+import {
+  Oswald_200ExtraLight,
+  Oswald_300Light,
+  Oswald_400Regular,
+  Oswald_500Medium,
+  Oswald_600SemiBold,
+  Oswald_700Bold,
+} from "@expo-google-fonts/oswald";
+
 LogBox.ignoreAllLogs();
 console.ignoredYellowBox = ["Setting a timer"];
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    Roboto_100Thin,
+    Roboto_100Thin_Italic,
+    Roboto_300Light,
+    Roboto_300Light_Italic,
+    Roboto_400Regular,
+    Roboto_400Regular_Italic,
+    Roboto_500Medium,
+    Roboto_500Medium_Italic,
+    // Roboto_700Bold,
+    Roboto_700Bold_Italic,
+    Roboto_900Black,
+    // Roboto_900Black_Italic,
+    Oswald_200ExtraLight,
+    Oswald_300Light,
+    Oswald_400Regular,
+    Oswald_500Medium,
+    Oswald_600SemiBold,
+    Oswald_700Bold,
+  });
   return (
     <Provider store={store}>
-
       <StatusBar style="dark" />
-      <RootStack />
-   </Provider>
+      {fontsLoaded ? <RootStack /> : null}
+    </Provider>
   );
 };
 export default App;
@@ -19,8 +63,8 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
